f0c33b790fc049c0afc5e32b0f54b05184377032
anshulahuja98 <anshul.ahu@gmail.com>
2018-06-26 14:06:21 +0530

    updated base,home, programs, gradesystem templates

--- src/main/urls.py
+++ src/main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+app_name = 'main'
+
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='main/index.html')),
-    path('gradesystem/', TemplateView.as_view(template_name='main/gradesystem.html')),
+    path('', TemplateView.as_view(template_name='main/index.html'), name ='home'),
+    path('gradesystem/', TemplateView.as_view(template_name='main/gradesystem.html'), name='gradesystem'),
     path('programs/', TemplateView.as_view(template_name='main/programs.html')),
     path('rules/', TemplateView.as_view(template_name='main/rules.html')),
     path('placementProcedure/', TemplateView.as_view(template_name='main/placementprocedure.html')),
