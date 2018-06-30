eac6b832e2c5ec96bbdb47a27e0347b860450022
anshulahuja98 <anshul.ahu@gmail.com>
2018-06-30 20:58:49 +0530

    dummy for testing

--- src/company/urls.py
+++ src/company/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import LoginView
+from django.views.generic import TemplateView
 
 app_name = 'company'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), {'next_page': ''}, name='login')
+    path('login/', LoginView.as_view(), name='login'),
+    path('dummy/', TemplateView.as_view(template_name="company/base.html"), name="dummy")
 ]
