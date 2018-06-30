709602204042f19a60a707f9644e10f97e27527c
anshulahuja98 <anshul.ahu@gmail.com>
2018-06-30 19:47:24 +0530

    details template for student
    - needs UI tweaking

--- src/student/urls.py
+++ src/student/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import LoginView
+from .views import LoginView, DetailsView, JobOffersListView, InternOffersListView, ResumeUploadView
+from django.contrib.auth.views import LogoutView
 
 app_name = 'student'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login')
+    path('login/', LoginView.as_view(), {'next_page': 'student:detail'}, name='login'),
+    path('details/', DetailsView.as_view(), name="detail"),
+    path('intern_offers/', InternOffersListView.as_view(), name='intern_offers'),
+    path('intern_offers/', JobOffersListView.as_view(), name='job_offers'),
+    path('resume_upload/', ResumeUploadView.as_view(), name='resume_upload'),
+    path('logout/', LogoutView.as_view(next_page='main:home'), name='logout'),
 ]
--- src/student/views.py
+++ src/student/views.py
@@ -1,6 +1,44 @@
 from django.contrib.auth.views import LoginView as DefaultLoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import DetailView, UpdateView, ListView, TemplateView
+from accounts.models import StudentProfile, Resume
+from company.models import JobOffer,InternOffer
+from django.shortcuts import get_object_or_404
+
+
+class UserObjectMixin(object):
+    def get_object(self):
+        return get_object_or_404(StudentProfile, user=self.request.user)
 
 
 class LoginView(DefaultLoginView):
     template_name = 'student/login.html'
 
+
+class DetailsView(UpdateView, LoginRequiredMixin, UserObjectMixin):
+    # template_name = 'student/details.html'
+    model = StudentProfile
+    fields = '__all__'
+    template_name = 'student/details.html'
+
+    def get_object(self):
+        return get_object_or_404(StudentProfile, user=self.request.user)
+
+
+class JobOffersListView(ListView, LoginRequiredMixin):
+    model = JobOffer
+    template_name = 'student/job_offers.html'
+
+    def get_queryset(self):
+        return self.model.objects
+
+class InternOffersListView(ListView, LoginRequiredMixin):
+    model = InternOffer
+    template_name = 'student/intern_offers.html'
+
+    def get_queryset(self):
+        return self.model.objects
+
+
+class ResumeUploadView(UpdateView):
+    model = Resume
