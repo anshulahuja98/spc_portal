96fdadda1255aadd1c1015394d5fb856a25afb5f
anshulahuja98 <anshul.ahu@gmail.com>
2018-07-01 18:26:47 +0530

    model changes of job and intern offer

--- src/company/models.py
+++ src/company/models.py
@@ -2,60 +2,58 @@ from django.db import models
 from accounts.models import CompanyProfile
 
 
-class JobOffer(models.Model):
-    company = models.ForeignKey(CompanyProfile, on_delete=models.CASCADE, related_name='joboffer',
-                                blank=True)
+class JobAdvertisement(models.Model):
     # job prof
-    job_designation = models.CharField(max_length=30)
-    job_description = models.CharField(max_length=50)
+    designation = models.CharField(max_length=30)
+    description = models.CharField(max_length=50)
     tentative_join_date = models.DateField()
-    tentative_job_loc = models.CharField(max_length=50)
+    tentative_job_location = models.CharField(max_length=50)
     ads = models.FileField(upload_to='ads')
     # package details
-    ctc = models.IntegerField
-    gross_sal = models.IntegerField
-    bonus = models.IntegerField(blank=True)
-    bond = models.BooleanField
+    ctc = models.IntegerField()
+    gross_salary = models.PositiveIntegerField()
+    bonus = models.PositiveIntegerField(blank=True, default=0)
+    bond = models.BooleanField()
     # selection process
-    resume = models.BooleanField
-    aptitude_test = models.BooleanField
-    technical_test = models.BooleanField
-    group_discussion = models.BooleanField
-    technical_interview = models.BooleanField
-    hr = models.BooleanField
-    medical = models.BooleanField
-    min_gpa = models.FloatField
+    resume_required = models.BooleanField()
+    aptitude_test_required = models.BooleanField()
+    technical_test_required = models.BooleanField()
+    group_discussion_required = models.BooleanField()
+    technical_interview_required = models.BooleanField()
+    hr_round_required = models.BooleanField()
+    medical_test_required = models.BooleanField()
+    min_gpa = models.FloatField()
     # logistic
-    number_members = models.IntegerField
-    other_details = models.TextField
+    number_of_members = models.PositiveIntegerField()
+    other_details = models.TextField()
 
 
-class InternOffer(models.Model):
-    company = models.ForeignKey(CompanyProfile, on_delete=models.CASCADE, related_name='internoffer',
-                                blank=True)
+class InternAdvertisement(models.Model):
+    # company = models.ForeignKey(CompanyProfile, on_delete=models.CASCADE, related_name='internoffer',
+    #                             blank=True)
     # job prof
-    job_designation = models.CharField(max_length=30)
-    job_description = models.CharField(max_length=50)
+    designation = models.CharField(max_length=30)
+    description = models.CharField(max_length=50)
     tentative_join_date = models.DateField()
-    tentative_job_loc = models.CharField(max_length=50)
+    tentative_job_location = models.CharField(max_length=50)
     ads = models.FileField(upload_to='ads')
     # package details
-    ctc = models.IntegerField
-    gross_sal = models.IntegerField
-    bonus = models.IntegerField(blank=True)
-    bond = models.BooleanField
+    ctc = models.IntegerField()
+    gross_salary = models.PositiveIntegerField()
+    bonus = models.PositiveIntegerField(blank=True, default=0)
+    bond = models.BooleanField()
     # selection process
-    resume = models.BooleanField
-    aptitude_test = models.BooleanField
-    technical_test = models.BooleanField
-    group_discussion = models.BooleanField
-    technical_interview = models.BooleanField
-    hr = models.BooleanField
-    medical = models.BooleanField
-    min_gpa = models.FloatField
+    resume_required = models.BooleanField()
+    aptitude_test_required = models.BooleanField()
+    technical_test_required = models.BooleanField()
+    group_discussion_required = models.BooleanField()
+    technical_interview_required = models.BooleanField()
+    hr_round_required = models.BooleanField()
+    medical_test_required = models.BooleanField()
+    min_gpa = models.FloatField()
     # logistic
-    number_members = models.IntegerField
-    other_details = models.TextField
+    number_of_members = models.PositiveIntegerField()
+    other_details = models.TextField()
 
 
 class Program(models.Model):
