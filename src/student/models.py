f293853bbc7230f22f24c1eed15554fe1decdf7a
anshulahuja98 <anshul.ahu@gmail.com>
2018-07-04 23:35:53 +0530

    abbreviation for program and branch models

--- src/student/models.py
+++ src/student/models.py
@@ -3,18 +3,20 @@ from django.db import models
 
 class Branch(models.Model):
     name = models.CharField(max_length=60)
+    abbreviation = models.CharField(max_length=10)
     usable = models.BooleanField(default=False)
 
     class Meta:
         verbose_name_plural = 'branches'
 
     def __str__(self):
-        return self.name
+        return self.abbreviation
 
 
 class Program(models.Model):
     name = models.CharField(max_length=60)
+    abbreviation = models.CharField(max_length=10)
     usable = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.name
+        return self.abbreviation
--- src/student/urls.py
+++ src/student/urls.py
@@ -6,6 +6,6 @@ app_name = 'student'
 urlpatterns = [
     path('details/', DetailsView.as_view(), name="detail"),
     path('intern_offers/', InternOffersListView.as_view(), name='intern-offers'),
-    path('intern_offers/', JobOffersListView.as_view(), name='job-offers'),
+    path('job_offers/', JobOffersListView.as_view(), name='job-offers'),
     path('resume_upload/', ResumeUploadView.as_view(), name='resume_upload'),
 ]
