4a64e5f692a8ef14afa564d5057d799998b7a537
anshulahuja98 <anshul.ahu@gmail.com>
2018-07-20 11:23:21 +0530

    basic registration view for company and student
    only added user fields, other fields left, also check password fields and validation

--- src/student/urls.py
+++ src/student/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import DetailsView, ResumeView, InternshipOffersView, JobOffersView
+from accounts.views import StudentRegisterFormView
 
 app_name = 'student'
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path('intern_offers/', InternshipOffersView.as_view(), name='intern-offers'),
     path('job_offers/', JobOffersView.as_view(), name='job-offers'),
     path('resume_upload/', ResumeView.as_view(), name='resume_upload'),
+    path('register/', StudentRegisterFormView.as_view(), name='register'),
 ]
