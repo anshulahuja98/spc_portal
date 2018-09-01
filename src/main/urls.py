d5e4ef09fd60db55f633cba91ecaeb02168c8a84
anshulahuja98 <anshul.ahu@gmail.com>
2018-09-02 01:14:27 +0530

    updated why recruit under recruiter tab in homepage nav
    removed  coe, research, facilities

--- src/main/urls.py
+++ src/main/urls.py
@@ -10,10 +10,6 @@ urlpatterns = [
     path('rules/', HomepageView.as_view(template_name='main/rules.html'), name='rules'),
     path('placementProcedure/', HomepageView.as_view(template_name='main/placementprocedure.html'),
          name='placementProcedure'),
-    path('newBreed/', HomepageView.as_view(template_name='main/newbreed.html'), name='newBreed'),
-    path('coe/', HomepageView.as_view(template_name='main/coe.html'), name='coe'),
-    path('research/', HomepageView.as_view(template_name='main/research.html'), name='research'),
-    path('facilities/', HomepageView.as_view(template_name='main/facilities.html'), name='facilities'),
     path('contacts/', HomepageView.as_view(template_name='main/contacts.html'), name='contacts'),
     path('admissions/', HomepageView.as_view(template_name='main/admissions.html'), name='admissions'),
     path('achievements/', HomepageView.as_view(template_name='main/achievements.html'), name='achievements'),
@@ -21,6 +17,7 @@ urlpatterns = [
     path('alumni/', HomepageView.as_view(template_name='main/alumni.html'), name='alumni'),
     path('student_rules/', HomepageView.as_view(template_name='main/student_rules.html'), name='student_rules'),
     path('internships/', HomepageView.as_view(template_name='main/internships.html'), name='internships'),
+    path('why_recruit/', HomepageView.as_view(template_name='main/why-recruit.html'), name='why-recruit'),
     path('brochure/', HomepageView.as_view(template_name='main/brochure.html'), name='brochure'),
     path('summary/', HomepageView.as_view(template_name='main/summary.html'), name='summary'),
     path('company_rules/', HomepageView.as_view(template_name='main/company_rules.html'), name='company_rules'),
@@ -33,6 +30,7 @@ urlpatterns = [
     path('dir_msg/', HomepageView.as_view(template_name='main/dir_msg.html'), name='dir_msg'),
     path('student/', include('student.urls')),
     path('company/', include('company.urls')),
-    path('tutorial_student/', HomepageView.as_view(template_name='main/tutorial-student.html'), name='tutorial_student'),
+    path('tutorial_student/', HomepageView.as_view(template_name='main/tutorial-student.html'),
+         name='tutorial_student'),
     path('tutorial_company/', HomepageView.as_view(template_name='main/tutorial-company.html'), name='tutorial_company')
 ]
