dad304bcf5672545ac42846b399805ae444df67e
anshulahuja98 <anshul.ahu@gmail.com>
2018-11-13 16:03:37 +0530

    Password Reset setup

--- src/spc_django/urls.py
+++ src/spc_django/urls.py
@@ -23,6 +23,8 @@ from django.conf.urls.static import static
 admin.site.site_title = 'SPC Administration'
 admin.site.site_header = 'SPC Administration'
 admin.site.index_title = 'Admin Panel'
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import PasswordResetView
 
 urlpatterns = [
                   path('login/', LoginView.as_view(), name='login'),
@@ -31,4 +33,11 @@ urlpatterns = [
                   path('company/', include('company.urls')),
                   path('student/', include('student.urls')),
                   path('', include('main.urls')),
+                  path('password_reset/',
+                       PasswordResetView.as_view(email_template_name='accounts/password_reset_email.html',
+                                                 subject_template_name='accounts/password_reset_subject.txt')),
+                  path('password_reset/done/', auth_views.password_reset_done, name='password_reset_done'),
+                  path('reset/<uidb64>[0-9A-Za-z_\-]+/<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20}/',
+                       auth_views.password_reset_confirm, name='password_reset_confirm'),
+                  path('reset/done/', auth_views.password_reset_complete, name='password_reset_complete'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
