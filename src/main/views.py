1738671d3d38da44eff9e69ae605fb3a05d8834a
Anshul Ahuja <anshul.ahu@gmail.com>
2019-04-04 10:42:50 +0530

    Order carousel images in desceneding order of date modified

--- src/main/views.py
+++ src/main/views.py
@@ -7,6 +7,9 @@ class HomepageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(HomepageView, self).get_context_data()
         carousel = os.listdir("staticfiles/img/homepage-carousel")
+        print(carousel)
+        carousel.sort(key=lambda x: os.path.getmtime('staticfiles/img/homepage-carousel/' + x), reverse=True)
+        print(carousel)
         carousel = ['img/homepage-carousel/' + image for image in carousel]
         context['carousel'] = carousel
         companies = os.listdir("staticfiles/img/company-logo")
