d6f519f9fd26f6593bb74bad2c60c8d0ba3a79dd
Anshul Ahuja <anshul.ahu@gmail.com>
2019-05-13 13:41:41 +0530

    Add null=True in news link and set active default to True

--- src/main/models.py
+++ src/main/models.py
@@ -5,10 +5,10 @@ class News(models.Model):
     title = models.CharField(max_length=64, blank=True)
     order_no = models.PositiveSmallIntegerField(default=512)
     content = models.TextField(max_length=512)
-    active = models.BooleanField()
+    active = models.BooleanField(default=True)
     document = models.FileField(upload_to='news', blank=True, null=True)
     file_title = models.CharField(max_length=64, default='Read More')
-    link = models.URLField(blank=True)
+    link = models.URLField(blank=True, null=True)
     link_title = models.CharField(max_length=64, default='Link')
 
     def __str__(self):
