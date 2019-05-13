deb79fea8a9ff8d3a551442152f61e5f77ee8ec4
Kaustubh Dixit <kdkishu335@gmail.com>
2019-05-13 13:35:25 +0530

    Removed unnecessary comments and imports.

--- src/main/views.py
+++ src/main/views.py
@@ -7,12 +7,6 @@ class HomepageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomepageView, self).get_context_data()
-        #carousel = os.listdir("staticfiles/img/homepage-carousel")
-        #print(carousel)
-        #carousel.sort(key=lambda x: os.path.getmtime('staticfiles/img/homepage-carousel/' + x), reverse=True)
-        #print(carousel)
-        #carousel = ['img/homepage-carousel/' + image for image in carousel]
-        #context['carousel'] = carousel
         context['carousel'] = HomeImageCarousel.objects.filter(active=True).order_by('ordering')
         companies = os.listdir("staticfiles/img/company-logo")
         companies = ['img/company-logo/' + image for image in companies]
