d13b6c857a50a088a413e39870db55ef49d4b1e4
Kaustubh Dixit <40864637+KD-3@users.noreply.github.com>
2019-05-17 19:08:29 +0530

    Add ability to update Homepage carousel from admin (#18)
    * Added Home Carousel.
    
    * Added Home Carousel.
    
    * Removed reindentation.

--- src/main/views.py
+++ src/main/views.py
@@ -1,16 +1,12 @@
 from django.views.generic import TemplateView
-import os
-from .models import PastRecruiters, CoreTeamContacts, Volunteers, News, AlumniTestimonial
+from .models import PastRecruiters, CoreTeamContacts, Volunteers, News, AlumniTestimonial, HomeImageCarousel
 
 
 class HomepageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomepageView, self).get_context_data()
-        carousel = os.listdir("staticfiles/img/homepage-carousel")
-        carousel.sort(key=lambda x: os.path.getmtime('staticfiles/img/homepage-carousel/' + x), reverse=True)
-        carousel = ['img/homepage-carousel/' + image for image in carousel]
-        context['carousel'] = carousel
+        context['carousel'] = HomeImageCarousel.objects.filter(active=True).order_by('ordering')
         context['news_list'] = News.objects.filter(active=True).order_by('order_no')
         context['companies'] = PastRecruiters.objects.filter(active=True).order_by('company_order_no')
         context['contacts'] = CoreTeamContacts.objects.filter(active=True).order_by('order_no')
