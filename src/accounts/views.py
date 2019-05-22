689d34e2023da25419610a1562c8899d17c8e15c
Tanuj22 <agarwal.5@iitj.ac.in>
2019-05-22 12:44:22 +0530

    modified accounts/views.py to send emails when a company/student registers

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -4,6 +4,9 @@ from django.views.generic import CreateView
 from .forms import StudentRegisterForm, CompanyRegisterForm
 from accounts.models import StudentProfile, CompanyProfile
 from main.views import HomepageView
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
+from django.template.loader import render_to_string
 
 
 class LoginView(DefaultLoginView, HomepageView):
@@ -58,6 +61,17 @@ class StudentRegisterFormView(CreateView):
                                                     std_image=kwargs['std_image'])
         userprofile.save()
 
+        # Sends email to the registered student
+        subject = "Registered with SPC"
+        from_email = settings.EMAIL_HOST_USER
+        to_email = [user.email, ]
+        with open(settings.BASE_DIR + "/templates/accounts/student_register_email.txt") as f:
+            register_message = f.read()
+        message = EmailMultiAlternatives(subject=subject, body=register_message, from_email=from_email, to=to_email)
+        html_template = render_to_string("accounts/student_register_email.html", {'username': user.username, 'email': user.email})
+        message.attach_alternative(html_template, "text/html")
+        message.send()
+
 
 class CompanyRegisterFormView(CreateView):
     model = CompanyProfile
@@ -72,9 +86,35 @@ class CompanyRegisterFormView(CreateView):
 
     @staticmethod
     def create_profile(user=None, **kwargs):
+
         # Creates a new UserProfile object after successful creation of User object
         userprofile = CompanyProfile.objects.create(user=user, name=kwargs['name'], domain=kwargs['domain'],
                                                     url=kwargs['url'], city=kwargs['city'], state=kwargs['state'],
                                                     country=kwargs['country'], pin_code=kwargs['pin_code'],
                                                     contact=kwargs['contact'], )
         userprofile.save()
+
+        # Sends email to the registered company
+        subject = "Registered with SPC"
+        from_email = settings.EMAIL_HOST_USER
+        to_email = [user.username, ]
+        with open(settings.BASE_DIR + "/templates/accounts/company_register_email.txt") as f:
+            register_message = f.read()
+        message = EmailMultiAlternatives(subject=subject, body=register_message, from_email=from_email, to=to_email)
+        html_template = render_to_string("accounts/company_register_email.html", {'username': user.username})
+        message.attach_alternative(html_template, "text/html")
+        message.send()
+
+        # Sends details of the registered company
+        subject1 = "Company Registered with SPC"
+        from_email1 = settings.EMAIL_HOST_USER
+        to_email1 = [settings.TO_EMAIL, ]
+        with open(settings.BASE_DIR + "/templates/accounts/company_details_email.txt") as f1:
+            details_message = f1.read()
+        message1 = EmailMultiAlternatives(subject=subject1, body=details_message, from_email=from_email1, to=to_email1)
+        html_template1 = render_to_string("accounts/company_details_email.html", {'name': kwargs['name'], 'email': user.username,
+                                                                                  'domain': kwargs['domain'], 'url': kwargs['url'],
+                                                                                  'city': kwargs['city'], 'state': kwargs['state'],
+                                                                                  'contact': kwargs['contact']})
+        message1.attach_alternative(html_template1, "text/html")
+        message1.send()
