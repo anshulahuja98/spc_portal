b9e699a555b2e566af210d0995b62c5484581a8c
Wolverin-e <patel.6@iitj.ac.in>
2019-06-12 22:09:47 +0530

    Added tests for URLs.

---
+++ src/spc_django/tests.py
@@ -0,0 +1,40 @@
+from django.test import SimpleTestCase, TestCase, Client
+from django.urls import resolve, reverse
+from django.contrib.auth.models import User
+from accounts.models import CompanyProfile
+from django.contrib.auth.views import LogoutView, PasswordResetView, password_reset_complete, \
+    password_reset_done
+
+
+class TestAccountsUrlsResolves(SimpleTestCase):
+
+    def test_logout_url_resolves(self):
+        url = reverse('logout')
+        self.assertEquals(resolve(url).func.view_class, LogoutView)
+
+    def test_password_reset_url_resolves(self):
+        url = reverse('password_reset')
+        self.assertAlmostEquals(resolve(url).func.view_class, PasswordResetView)
+
+    def test_password_reset_done_url_resolves(self):
+        url = reverse('password_reset_done')
+        self.assertAlmostEquals(resolve(url).func, password_reset_done)
+
+    def test_password_reset_complete_url_resolves(self):
+        url = reverse('password_reset_complete')
+        self.assertAlmostEquals(resolve(url).func, password_reset_complete)
+
+
+class TestAccountsUrlsResolves1(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.login_url = reverse('login')
+        self.test_user = User.objects.create_user(username='testuser', password='AlphaBeta124')
+
+    def test_login_redirects_to_supposed_url(self):
+        self.company_test_user = CompanyProfile.objects.create(name='Test', user=self.test_user, domain='Consulting',
+                                                               country='Indian', contact=1234567890)
+        self.client.login(username='testuser', password='AlphaBeta124')
+        response = self.client.get(reverse('login'), follow=True)
+        self.assertRedirects(response, reverse('company:job-offers-added'))
