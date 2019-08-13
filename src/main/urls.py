bb36d3f2c648c687d3812dba612534a4e15aa83b
Tanuj22 <42486853+Tanuj22@users.noreply.github.com>
2019-08-13 17:57:56 +0530

    added urls for turorial pages (#41)

--- src/main/urls.py
+++ src/main/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('', HomepageView.as_view(template_name='main/index.html'), name='home'),
     path('student/', include('student.urls')),
     path('company/', include('company.urls')),
+    path('tutorial_student/', HomepageView.as_view(template_name='main/tutorial-student.html'),
+         name='tutorial_student'),
+    path('tutorial_company/', HomepageView.as_view(template_name='main/tutorial-company.html'), name='tutorial_company'),
     path('<slug:slug>/', NavBarSubOptionsPageView.as_view(), name='navbarsuboptionpage')
 ]
