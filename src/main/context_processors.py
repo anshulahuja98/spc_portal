1edf7bb1d0f36bf062ff1a62f4db9d465534cebc
Tanuj22 <agarwal.5@iitj.ac.in>
2019-08-16 15:19:55 +0530

    order_no field added to nav bar options

--- src/main/context_processors.py
+++ src/main/context_processors.py
@@ -3,6 +3,6 @@ from .models import NavBarOptions, PastRecruiters
 
 def add_variable_to_context(request):
     return {
-        'navbaroptions': NavBarOptions.objects.filter(active=True),
+        'navbaroptions': NavBarOptions.objects.filter(active=True).order_by('order_no'),
         'companies': PastRecruiters.objects.filter(active=True).order_by('company_order_no')
     }
