52c50c87f2b41923f953fb587cad25609036aab8
Anshul Ahuja <anshul.ahu@gmail.com>
2019-12-09 10:20:28 +0530

    Update models.py

--- src/main/models.py
+++ src/main/models.py
@@ -12,7 +12,7 @@ import re
 class News(models.Model):
     title = models.CharField(max_length=64, blank=True)
     order_no = models.PositiveSmallIntegerField(default=512)
-    content = models.TextField(max_length=512)
+    content =  models.TextField(max_length=512)
     active = models.BooleanField(default=True)
     document = models.FileField(upload_to='news', blank=True, null=True)
     file_title = models.CharField(max_length=64, default='Read More')
