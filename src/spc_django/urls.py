753582093b390736a4ee9922fa101695f459a1ce
DarshitJain04 <jain.38@iitj.ac.in>
2020-06-20 23:19:07 +0530

    Moved password_reset templates to accounts folder

--- src/spc_django/urls.py
+++ src/spc_django/urls.py
@@ -35,21 +35,21 @@ urlpatterns = [
     path(
         'password_reset/',
         PasswordResetView.as_view(
-            template_name='student/password_reset.html',
+            template_name='accounts/password_reset.html',
             html_email_template_name='accounts/password_reset_email.html',
             subject_template_name='accounts/password_reset_subject.txt'),
         name='password_reset'),
     path(
         'password_reset/done/',
-        PasswordResetDoneView.as_view(template_name='student/password_reset_done.html'),
+        PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),
         name='password_reset_done'),
     path(
         'reset/<uidb64>[0-9A-Za-z_-]+/<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20}/',
-        PasswordResetConfirmView.as_view(template_name='student/password_reset_confirm.html'),
+        PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'),
         name='password_reset_confirm'),
     path(
         'reset/done/',
-        PasswordResetCompleteView.as_view(template_name='student/password_reset_complete.html'),
+        PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
         name='password_reset_complete'),
     path('', include('main.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
