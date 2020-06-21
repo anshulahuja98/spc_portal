a30d86d4d33c7e256a5cacfec0ee4492d72f6f4b
DarshitJain04 <jain.38@iitj.ac.in>
2020-06-21 15:00:00 +0530

    removed password_reset tests

--- src/spc_django/tests.py
+++ src/spc_django/tests.py
@@ -2,8 +2,7 @@ from django.test import SimpleTestCase, TestCase, Client
 from django.urls import resolve, reverse
 from django.contrib.auth.models import User
 from accounts.models import CompanyProfile
-from django.contrib.auth.views import LogoutView, PasswordResetView, password_reset_complete, \
-    password_reset_done
+from django.contrib.auth.views import LogoutView
 
 
 class TestAccountsUrlsResolves(SimpleTestCase):
@@ -11,18 +10,6 @@ class TestAccountsUrlsResolves(SimpleTestCase):
     def test_logout_url_resolves(self):
         url = reverse('logout')
         self.assertEquals(resolve(url).func.view_class, LogoutView)
-
-    def test_password_reset_url_resolves(self):
-        url = reverse('password_reset')
-        self.assertAlmostEquals(resolve(url).func.view_class, PasswordResetView)
-
-    def test_password_reset_done_url_resolves(self):
-        url = reverse('password_reset_done')
-        self.assertAlmostEquals(resolve(url).func, password_reset_done)
-
-    def test_password_reset_complete_url_resolves(self):
-        url = reverse('password_reset_complete')
-        self.assertAlmostEquals(resolve(url).func, password_reset_complete)
 
 
 class TestAccountsUrlsResolves1(TestCase):
