a75c88bed462d397011de878e5e6fc1e02489bc4
Sahil Harpal <56965873+Sahil1479@users.noreply.github.com>
2020-12-30 17:01:30 +0530

    Added UG gpa in Job/Intern offers export

--- src/company/resources.py
+++ src/company/resources.py
@@ -95,7 +95,7 @@ class BaseOfferResource(resources.ModelResource):
         abstract = True
         fields = ('company', 'is_accepted', 'ppo',)
         export_order = ('student_applied_offers', 'resume_link',
-                        'company', 'designation', 'roll_no', 'name', 'email', 'dob', 'year', 'program_branch', 'gpa','ug_gpa',
+                        'company', 'designation', 'roll_no', 'name', 'email', 'dob', 'year', 'program_branch', 'gpa', 'ug_gpa',
                         'phone',
                         'category',
                         'jee_air', 'x_year', 'x_board_name', 'x_percentage', 'xii_year', 'xii_board_name',
