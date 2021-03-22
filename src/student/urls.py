6ba632f77d72f9f83584bb52e8991421518a304e
Sahil Harpal <56965873+Sahil1479@users.noreply.github.com>
2021-03-22 18:34:27 +0530

    Added feedback url

--- src/student/urls.py
+++ src/student/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import DetailsView, ResumeView, InternshipOffersView, JobOffersView
+from .views import DetailsView, ResumeView, InternshipOffersView, JobOffersView, FeedbackFormView
 from accounts.views import StudentRegisterFormView
 
 app_name = 'student'
@@ -10,4 +10,5 @@ urlpatterns = [
     path('job_offers/', JobOffersView.as_view(), name='job-offers'),
     path('resume_upload/', ResumeView.as_view(), name='resume_upload'),
     path('register/', StudentRegisterFormView.as_view(), name='register'),
+    path('feedback/', FeedbackFormView.as_view(), name='feedback'),
 ]
